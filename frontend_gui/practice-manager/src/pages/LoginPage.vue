b91997c0c9fbebf06a6a788b6e7a99600997b4ec
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-06-25 19:25:02 +0100

    scaffold a login view

--- frontend_gui/practice-manager/src/pages/LoginPage.vue
+++ frontend_gui/practice-manager/src/pages/LoginPage.vue
@@ -1,8 +1,16 @@
 <template>
-  <div id="login">
-      <NavBar/>
-    <h1>Login Page</h1>
-  </div>
+    <section>
+        <NavBar/>
+        <h1>Login</h1>
+        <section class="login-form">
+            <b-field label="Username" style="text-align:left">
+                <b-input value=""></b-input>
+            </b-field>
+            <b-field label="Password" style="text-align:left" >
+                <b-input type="password" maxlength="30"></b-input>
+            </b-field>
+        </section>
+    </section>
 </template>
 
 <script>
@@ -17,13 +25,21 @@ export default {
 }
 </script>
 
-<style>
-#login {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
+<style scoped>
+.login-form {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    max-width: 500px;
+    margin: 0 auto;
+    margin-top: 20px;
+    padding: 20px 15px 20px 15px;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+}
+h1 {
+    margin-top: 100px;
+    font-size: 20pt;
 }
 </style>
