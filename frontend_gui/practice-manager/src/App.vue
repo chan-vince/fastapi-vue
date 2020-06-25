5323c305c75b690ecd25d160d2b5cca2f036ac01
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-06-25 18:08:24 +0100

    initial play with vue

--- frontend_gui/practice-manager/src/App.vue
+++ frontend_gui/practice-manager/src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar/>
+    <h1 class="subsection">GP Practices</h1>
+    <Table/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Table from './components/Table.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    NavBar,
+    Table
   }
 }
 </script>
 
 <style>
+@import 'https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+h1.subsection {
+  font-weight: bold;
+  font-size: 30pt;
+  text-align: left;
+  padding-top: 30px;
+  padding-bottom: 10px;
+  padding-left: 30px;
 }
 </style>
