c6dc22381254b54bc5ea8c72b6af2ec36f5c13ec
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-06-26 00:17:45 +0100

    nicer table view for gp practices. still need to work out pagination

--- frontend_gui/practice-manager/src/components/Table.vue
+++ frontend_gui/practice-manager/src/components/Table.vue
@@ -2,41 +2,12 @@
     <div class="table-div">
         <section>
             <b-field grouped group-multiline>
-                <!-- <b-select v-model="defaultSortDirection">
-                    <option value="asc">Sort Ascending</option>
-                    <option value="desc">Sort Descending</option>
-                </b-select> -->
                 <b-select v-model="perPage" :disabled="!isPaginated">
                     <option value="5">5 per page</option>
                     <option value="10">10 per page</option>
                     <option value="15">15 per page</option>
                     <option value="20">20 per page</option>
                 </b-select>
-                <!-- <div class="control">
-                    <button class="button" @click="currentPage = 2" :disabled="!isPaginated">Set page to 2</button>
-                </div> -->
-                <!-- <div class="control is-flex">
-                    <b-switch v-model="isPaginated">Paginated</b-switch>
-                </div>
-                <div class="control is-flex">
-                    <b-switch v-model="isPaginationSimple" :disabled="!isPaginated">Simple pagination</b-switch>
-                </div> -->
-                <!-- <b-select v-model="paginationPosition" :disabled="!isPaginated">
-                    <option value="bottom">bottom pagination</option>
-                    <option value="top">top pagination</option>
-                    <option value="both">both</option>
-                </b-select> -->
-                <!-- <b-select v-model="sortIcon">
-                    <option value="arrow-up">Arrow sort icon</option>
-                    <option value="menu-up">Caret sort icon</option>
-                    <option value="chevron-up">Chevron sort icon </option>
-                </b-select>
-                <b-select v-model="sortIconSize">
-                    <option value="is-small">Small sort icon</option>
-                    <option value="">Regular sort icon</option>
-                    <option value="is-medium">Medium sort icon</option>
-                    <option value="is-large">Large sort icon</option>
-                </b-select> -->
             </b-field>
 
             <b-table
@@ -49,7 +20,7 @@
                 :default-sort-direction="defaultSortDirection"
                 :sort-icon="sortIcon"
                 :sort-icon-size="sortIconSize"
-                default-sort="user.first_name"
+                default-sort="name"
                 aria-next-label="Next page"
                 aria-previous-label="Previous page"
                 aria-page-label="Page"
@@ -60,28 +31,34 @@
                         {{ props.row.id }}
                     </b-table-column>
 
-                    <b-table-column field="user.first_name" label="First Name" sortable>
-                        {{ props.row.user.first_name }}
+                    <b-table-column field="created_date" label="Date Created" sortable>
+                        {{ displayDate(props.row.created_date) }}
                     </b-table-column>
 
-                    <b-table-column field="user.last_name" label="Last Name" sortable>
-                        {{ props.row.user.last_name }}
+                    <b-table-column field="name" label="Practice Name" sortable>
+                        {{ props.row.name }}
                     </b-table-column>
 
-                    <b-table-column field="date" label="Date" sortable centered>
-                        <span class="tag is-success">
-                            {{ new Date(props.row.date).toLocaleDateString() }}
-                        </span>
+                    <b-table-column field="national_code" label="National Code" sortable>
+                        {{ props.row.national_code }}
                     </b-table-column>
 
-                    <b-table-column label="Gender">
-                        <span>
-                            <b-icon pack="fas"
-                                :icon="props.row.gender === 'Male' ? 'mars' : 'venus'">
-                            </b-icon>
-                            {{ props.row.gender }}
-                        </span>
+                    <b-table-column field="emis_cdb_practice_code" label="EMIS CDB Practice Code" sortable>
+                        {{ props.row.emis_cdb_practice_code }}
                     </b-table-column>
+
+                     <b-table-column field="access_systems" label="Access Systems">
+                        {{props.row.access_systems.map(a => a.name).join(", ")}}
+                    </b-table-column>
+
+                    <b-table-column field="ip_ranges" label="IP Ranges">
+                        {{props.row.ip_ranges.map(a => a.cidr).join(", ")}}
+                    </b-table-column>
+
+                    <b-table-column field="phone_num" label="Phone Number">
+                        {{ props.row.phone_num }}
+                    </b-table-column>
+
                 </template>
             </b-table>
         </section>
@@ -89,12 +66,13 @@
 </template>
 
 <script>
-    const data = require('../../data/sample.json')
+    import {client} from '../api.js'
+    import moment from 'moment'
 
     export default {
         data() {
             return {
-                data,
+                data: [],
                 isPaginated: true,
                 isPaginationSimple: false,
                 paginationPosition: 'bottom',
@@ -102,7 +80,18 @@
                 sortIcon: 'chevron-up',
                 sortIconSize: 'is-small',
                 currentPage: 1,
-                perPage: 15
+                perPage: 20
+            }
+        },
+        created () {    
+            client.get(`api/v1/practice/`, {params: { skip: 0, limit: 50 }})
+            .then(response => {
+                this.data = response.data
+            })
+        },
+        methods: {
+            displayDate (datestring) {
+                return moment(datestring).format("Do MMM YYYY")
             }
         }
     }
