e664b6456210999ceda7b0ef690b2a38a2fc15e9
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-06-26 01:10:28 +0100

    padding for navbar

--- frontend_gui/practice-manager/src/components/NavBar.vue
+++ frontend_gui/practice-manager/src/components/NavBar.vue
@@ -1,53 +1,55 @@
 <template>
-    <b-navbar>
-        <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="../../public/logo.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
-                >
-            </b-navbar-item>
-        </template>
-        <template slot="start">
-            <b-navbar-item v-show="isAdmin" href="#">
-                Access Systems
-            </b-navbar-item>
-            <b-navbar-item v-show="isAdmin" href="#">
-                Pending Approvals
-            </b-navbar-item>
-            <b-navbar-dropdown v-show="isAdmin" label="Practices">
-                <b-navbar-item href="#">
-                    Add New Practice
+    <div style="padding-left: 20px">
+        <b-navbar>
+            <template slot="brand">
+                <b-navbar-item tag="router-link" :to="{ path: '/admin' }" style="padding-right: 40px">
+                    <img
+                        src="../../public/logo.png"
+                        alt="Lightweight UI components for Vue.js based on Bulma"
+                    >
                 </b-navbar-item>
-                <b-navbar-item href="#">
-                    Edit Practice Details
+            </template>
+            <template slot="start">
+                <b-navbar-item v-show="isAdmin" href="#">
+                    Access Systems
                 </b-navbar-item>
-            </b-navbar-dropdown>
-            <b-navbar-dropdown v-show="isAdmin" label="Employees">
-                <b-navbar-item href="#">
-                    Add New Employee
+                <b-navbar-item v-show="isAdmin" href="#">
+                    Pending Approvals
                 </b-navbar-item>
-                <b-navbar-item href="#">
-                    Edit Employee Details
-                </b-navbar-item>
-            </b-navbar-dropdown>
-        </template>
+                <b-navbar-dropdown v-show="isAdmin" label="Practices">
+                    <b-navbar-item href="#">
+                        Add New Practice
+                    </b-navbar-item>
+                    <b-navbar-item href="#">
+                        Edit Practice Details
+                    </b-navbar-item>
+                </b-navbar-dropdown>
+                <b-navbar-dropdown v-show="isAdmin" label="Employees">
+                    <b-navbar-item href="#">
+                        Add New Employee
+                    </b-navbar-item>
+                    <b-navbar-item href="#">
+                        Edit Employee Details
+                    </b-navbar-item>
+                </b-navbar-dropdown>
+            </template>
 
-        <template slot="end">
-            <b-navbar-dropdown label="Switch View">
-                <router-link to='/login'><b-navbar-item>Login</b-navbar-item></router-link>
-                <router-link to='/admin'><b-navbar-item>Admin</b-navbar-item></router-link>
-                <router-link to='/practice'><b-navbar-item>GP Practice</b-navbar-item></router-link>
-            </b-navbar-dropdown>
-            <!-- <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-light">
-                        Report Issue
-                    </a>
-                </div>
-            </b-navbar-item> -->
-        </template>
-    </b-navbar>
+            <template slot="end">
+                <b-navbar-dropdown label="Switch View">
+                    <router-link to='/login'><b-navbar-item>Login</b-navbar-item></router-link>
+                    <router-link to='/admin'><b-navbar-item>Admin</b-navbar-item></router-link>
+                    <router-link to='/practice'><b-navbar-item>GP Practice</b-navbar-item></router-link>
+                </b-navbar-dropdown>
+                <!-- <b-navbar-item tag="div">
+                    <div class="buttons">
+                        <a class="button is-light">
+                            Report Issue
+                        </a>
+                    </div>
+                </b-navbar-item> -->
+            </template>
+        </b-navbar>
+    </div>
 </template>
 
 <script>
