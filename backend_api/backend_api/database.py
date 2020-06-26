219949dad672be7ea9f4b72c4bf6815bdb33b21c
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-06-26 14:51:05 +0100

    fix index creation and switch back to mariadb

--- backend_api/backend_api/database.py
+++ backend_api/backend_api/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://francis:***REMOVED***@localhost/francis"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://francis:***REMOVED***@127.0.0.1:3306/francis"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
