f4b6e8986bbad1587cfdfad76963f205b8b206e4
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-06-27 23:15:16 +0100

    fix database timeouts with mariadb

--- backend_api/backend_api/database.py
+++ backend_api/backend_api/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://francis:***REMOVED***@127.0.0.1:3306/francis"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_timeout=60, pool_recycle=120)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
