656defbd8892d20580146c97c478ddfaa69eb962
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-07-02 00:19:54 +0100

    dynamic window sizing improvements

--- frontend_gui/practice-manager/src/pages/PendingApprovalsPage.vue
+++ frontend_gui/practice-manager/src/pages/PendingApprovalsPage.vue
@@ -1,31 +1,33 @@
 <template>
     <div>
-      <NavBar/>
-      <div class=card id=spacing-margins-card>
-          <b-tabs id=spacing-margins-tabs>
-            <b-tab-item label="Practices">
-                <br>
-                <div class="card-header-title">
-                    Pending Approvals
-                </div>
-                <div class="card">
-                    <PracticeApprovalsTable v-bind:pendingOnly="true" style="margin: 0px 20px 120px 20px"/>
-                </div>
-                
-                <hr>
-                <div class=card-header-title>
-                    Approval History
-                </div>
-                <div class="card">
-                    <PracticeApprovalsTable v-bind:pendingOnly="false" style="margin: 0px 20px 0px 20px"/>
-                </div>
-            </b-tab-item>
-            <b-tab-item label="Employees">
-                insert employees table
-            </b-tab-item>
-          </b-tabs>
-      </div>
-  </div>
+        <NavBar/>
+        <div class="container" style="max-width: 1920px">
+            <div class="card" id=spacing-margins-card>
+                <b-tabs id=spacing-margins-tabs>
+                    <b-tab-item label="Practices">
+                        <br>
+                        <div class="card-header-title">
+                            Pending Approvals
+                        </div>
+                        <div class="card">
+                            <PracticeApprovalsTable v-bind:pendingOnly="true" style="margin: 0px 20px 120px 20px"/>
+                        </div>
+                        
+                        <hr>
+                        <div class=card-header-title>
+                            Approval History
+                        </div>
+                        <div class="card">
+                            <PracticeApprovalsTable v-bind:pendingOnly="false" style="margin: 0px 20px 0px 20px"/>
+                        </div>
+                    </b-tab-item>
+                    <b-tab-item label="Employees">
+                        insert employees table
+                    </b-tab-item>
+                </b-tabs>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -50,10 +52,10 @@ export default {
 <style scoped>
 
 #spacing-margins-card {
-    margin: 40px 40px 40px 40px;
+    margin: 40px 0px 40px 0px;
 }
 #spacing-margins-tabs {
-    margin: 40px 10px 40px 10px;
+    margin: 40px 20px 40px 20px;
 }
 
 </style>
