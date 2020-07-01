5fce9cdc1559c6f07aa231ca4dd335e6e81477f7
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-07-01 14:32:47 +0100

    remove sql db passwords with bfg

--- backend_api/backend_api/database.py
+++ backend_api/backend_api/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://francis:***REMOVED***@127.0.0.1:3306/francis"
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://francis:{MYSQL_PASSWORD}@127.0.0.1:3306/francis"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_timeout=60, pool_recycle=120)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
