3207456449b7188dff477e69b075e76a0d99edc8
Vince Chan <vince.chan@mclaren.com>
2020-07-03 20:06:30 +0100

    filled out tables for employees table

--- frontend_gui/practice-manager/src/pages/AllEmployeesPage.vue
+++ frontend_gui/practice-manager/src/pages/AllEmployeesPage.vue
@@ -42,11 +42,41 @@
         @dblclick="onDoubleClick"
       >
         <template slot-scope="props">
+          <b-table-column field="id" label="ID" sortable>{{ props.row.id }}</b-table-column>
+          <b-table-column field="name" label="Name" sortable>{{ props.row.name }}</b-table-column>
           <b-table-column
-            field="name"
-            label="Name"
+            field="job_title"
+            label="Job Title"
             sortable
-          >{{ props.row.name }}</b-table-column>
+          >{{ props.row.job_title.title }}</b-table-column>
+          <b-table-column
+            field="email"
+            label="Email"
+            sortable
+          >{{ props.row.email }}</b-table-column>
+          <b-table-column
+            field="professional_num"
+            label="Professional ID"
+            sortable
+          >{{ props.row.professional_num }}</b-table-column>
+          <b-table-column
+            field="it_portal_num"
+            label="IT Portal ID"
+            sortable
+          >{{ props.row.it_portal_num }}</b-table-column>
+          <b-table-column
+            field="desktop_num"
+            label="Desktop ID"
+            sortable
+          >{{ props.row.desktop_num }}</b-table-column>
+          <b-table-column field="active" label="Active" sortable>
+            <template v-if="props.row.active == true">
+              <b-icon icon="check"></b-icon>
+            </template>
+            <template v-else>
+              <b-icon icon="close"></b-icon>
+            </template>
+          </b-table-column>
         </template>
       </b-table>
     </div>
@@ -72,7 +102,7 @@ export default {
       employee_names: [],
       loading: false,
       isPaginated: true,
-      isPaginationSimple: false,
+      isPaginationSimple: true,
       paginationPosition: "bottom",
       defaultSortDirection: "asc",
       sortIcon: "chevron-up",
@@ -117,7 +147,6 @@ export default {
         );
         var employee_details = [];
         var promises = [];
-        console.log(names)
         for (const name of names) {
           promises.push(
             client
@@ -152,7 +181,6 @@ export default {
       });
     },
     updateTable(searchInput) {
-      console.log(searchInput);
       this.searchInput = searchInput;
       this.filterEmployees();
     }
