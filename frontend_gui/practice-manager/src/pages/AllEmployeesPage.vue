4451d0970b5865b0e61dd0305b6f7be4d956ff55
Vince Chan <vince.chan@mclaren.com>
2020-07-03 18:27:29 +0100

    add route for employees table

---
+++ frontend_gui/practice-manager/src/pages/AllEmployeesPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <div class="container" style="max-width: 1920px">
+      <PracticesTable/>
+    </div>
+  </div>
+</template>
+
+<script>
+import PracticesTable from '../components/PracticesTable.vue'
+import NavBar from '../components/NavBar.vue'
+
+export default {
+  name: 'AllPracticesPage',
+  components: {
+    NavBar,
+    PracticesTable
+  }
+}
+</script>
+
+<style>
+@import 'https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+
+</style>
