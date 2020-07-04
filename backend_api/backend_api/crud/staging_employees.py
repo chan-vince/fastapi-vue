5ed3f85c311efbff2b4e82ed5166fb15f166307d
Vince Chan <vince.chan@mclaren.com>
2020-07-05 00:11:43 +0100

    find by an id instead of source id

--- backend_api/backend_api/crud/staging_employees.py
+++ backend_api/backend_api/crud/staging_employees.py
@@ -18,8 +18,9 @@ def object_as_dict(obj):
 def update_staging_employee(db: Session, staging_employee: schemas.StagingEmployeeCreateRequest):
 
     existing_record_query = db.query(tables.StagingEmployee)\
-        .filter(tables.StagingEmployee.source_id == staging_employee.source_id)\
+        .filter(tables.StagingEmployee.id == staging_employee.id)\
         .filter(tables.StagingEmployee.approved == None)
+
     existing_record = existing_record_query.first()
 
     # If there's no existing staging record, create and insert one
