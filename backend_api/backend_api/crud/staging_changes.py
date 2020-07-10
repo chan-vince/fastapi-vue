3cab8edc16b402a63b76c87347c91ad152589c41
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-07-11 00:30:35 +0100

    rest and crud for new staging table

--- backend_api/backend_api/crud/staging_changes.py
+++ backend_api/backend_api/crud/staging_changes.py
@@ -1,12 +1,110 @@
-from sqlalchemy.orm import Session
+import logging
+
 import sqlalchemy.exc
+from sqlalchemy import inspect
+from sqlalchemy.orm import Session
+
 import backend_api.exc
+from backend_api import database_models as tables
+from backend_api.database import Base
+from backend_api.pydantic_schemas import StagingChangeRequest
+
+logger = logging.getLogger("StagingChanges")
+
+
+def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key)
+            for c in inspect(obj).mapper.column_attrs}
 
 
 def id_exists(db: Session, table: str, id: int):
-    pass
+    for table_model in Base._decl_class_registry.values():
+        if hasattr(table_model, '__tablename__') and table_model.__tablename__ == table:
+            if db.query(table_model).filter(table_model.id == id).first() == None:
+                return False
+            return True
+    else:
+        return False
 
-    # table = declarative_base().metadata.tables[table]
-    # if db.query(table).filter(table.id == id).all() == None:
-    #     return False
-    # return True
+
+def get_table_model_by_name(table: str):
+    for table_model in Base._decl_class_registry.values():
+        if hasattr(table_model, '__tablename__') and table_model.__tablename__ == table:
+            return table_model
+    else:
+        return None
+
+
+def create_staging_record(db: Session, request: StagingChangeRequest):
+
+    if request.modify:
+        raise AssertionError("Cannot create a new record if the modify flag is true")
+
+    record = tables.StagingChanges(**request.dict())
+    db.add(record)
+
+    try:
+        db.commit()
+        return record
+
+    except sqlalchemy.exc.IntegrityError:
+        raise backend_api.exc.DuplicateStagingChangePayload
+
+
+def modify_staging_record(db: Session, request: StagingChangeRequest):
+
+    if not request.modify:
+        raise AssertionError("Modify requests should set modify to true")
+
+    if request.target_id is None:
+        raise AssertionError("target_id is required")
+
+    table_model = get_table_model_by_name(request.target_table)
+
+    record_query = db.query(table_model)\
+        .filter(table_model.id == request.target_id)
+    record = record_query.first()
+
+    # the below for when we actually this is request and update the real record
+    # Look for pending (approved = null) records which have a matching target table and id
+    # record_query = db.query(tables.StagingChanges)\
+    #     .filter(tables.StagingChanges.approved == None)\
+    #     .filter(tables.StagingChanges.target_table == request.target_table)\
+    #     .filter(tables.StagingChanges.target_id == request.target_id)
+
+    if record is None:
+        raise backend_api.exc.StagingChangeNotFoundError
+
+    staging_record = tables.StagingChanges(**request.dict())
+
+    # check for deltas, as potentially nothing actually changed
+    for key, value in (request.payload.dict()).items():
+        if record.__dict__.get(key) != value:
+            logger.debug(f"Found a delta for {key}. Before: {record.__dict__.get(key)} After: {value}")
+            break
+    else:
+        logger.debug("Request payload is identical to current state. Nothing to do")
+        return None
+
+    db.add(staging_record)
+
+    try:
+        db.commit()
+        return staging_record
+    except sqlalchemy.exc.IntegrityError:
+        db.rollback()
+        return db.query(tables.StagingChanges)\
+            .filter(tables.StagingChanges.target_table == request.target_table)\
+            .filter(tables.StagingChanges.target_id == request.target_id)\
+            .first()
+
+
+def read_all_staging_records(db: Session, skip: int, limit: int):
+    return db.query(tables.StagingChanges).offset(skip).limit(limit).all()
+
+
+def get_delta_for_record(db: Session, staging_id: int):
+    pass
+    # get the staging record
+    # lookup the table/id to get the real data
+    # for each of the staging record payload entries, get the
--- backend_api/backend_api/rest/staging_changes.py
+++ backend_api/backend_api/rest/staging_changes.py
@@ -7,8 +7,9 @@ from sqlalchemy.orm import Session
 
 import backend_api.pydantic_schemas as schemas
 import backend_api.exc
-# from backend_api.crud.staging_changes import *
+from backend_api.crud.staging_changes import id_exists
 from backend_api.database import get_db, table_names, printable_tables
+from backend_api.crud import staging_changes as crud
 
 logger = logging.getLogger("REST:StagingEmployees")
 
@@ -16,25 +17,62 @@ logger = logging.getLogger("REST:StagingEmployees")
 router = APIRouter()
 
 
-@router.put("/ip_range", response_model=schemas.StagingChangeResponse)
-def change_existing_ip_range(request: schemas.StagingChangeRequest, db: Session = Depends(get_db)):
+@router.post("/", response_model=schemas.StagingChangeResponse)
+def create_new_record_request(request: schemas.StagingChangeRequest, db: Session = Depends(get_db)):
     """
-    Modify an existing IP Range
-    :param request: the request body must contain the keys defined in the StagingChangeRequest schema
-    :param db:
-    :return: schemas.StagingChangeResponse
+    Stores a record in the staging changes table, which details which table to put this new payload in. Don't forget
+    the "payload" object, which for some reason is broken if you're viewing this from the docs page
     """
 
     # Ensure the target table exists
     if request.target_table not in table_names:
         raise HTTPException(status_code=400, detail=f"Invalid target_table, must be one of {printable_tables}")
 
-    # Ensure that target_id is not set if the modify flag is false
-    if not request.modify and request.target_id is not None:
-        raise HTTPException(status_code=400, detail=f"Do not specify a target_id if you intend to create a new entry")
+    # Ensure that target_id is not set since we're not modifying
+    if request.modify or request.target_id is not None:
+        raise HTTPException(status_code=400, detail=f"target_id is set or modify is true. Use the PUT request to modify an existing entry")
 
-    # # If the modify flag is true, then an existing record must exist
+    # # # If the modify flag is true, then an existing record must exist
     # if request.modify and not id_exists(db, request.target_table, request.target_id):
     #     raise HTTPException(status_code=400, detail=f"No record in {request.target_table} with ID {request.target_id}")
 
-    return None
+    try:
+        record = crud.create_staging_record(db, request)
+        return record
+
+    except AssertionError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except backend_api.exc.DuplicateStagingChangePayload:
+        raise HTTPException(status_code=409, detail=f"A request already exists with payload: {request.payload}")
+
+
+@router.put("/", response_model=Union[schemas.StagingChangeResponse, None])
+def modify_existing_record_request(request: schemas.StagingChangeRequest, db: Session = Depends(get_db)):
+
+    # Ensure the target table exists
+    if request.target_table not in table_names:
+        raise HTTPException(status_code=400, detail=f"Invalid target_table, must be one of {printable_tables}")
+
+    # Ensure the right parameters are set
+    if not request.modify or request.target_id is None:
+        raise HTTPException(status_code=400, detail=f"To modify a request, set modify to true and set target_id to a row id")
+
+    return crud.modify_staging_record(db, request)
+    try:
+        return crud.modify_staging_record(db, request)
+    except AssertionError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except backend_api.exc.StagingChangeNotFoundError:
+        raise HTTPException(status_code=409, detail=f"No entry exists with id {request.target_id} in table {request.target_table}")
+    # except backend_api.exc.StagingChangeNoEffectError as e:
+        # return HTTPException(status_code=218, detail=e.args[0])
+
+
+@router.get("/", response_model=List[schemas.StagingChangeResponse])
+def get_all_staging_records(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.read_all_staging_records(db, skip=skip, limit=limit)
+
+
+@router.get("/delta", response_model=schemas.StagingChangeDeltaResponse)
+def get_delta_between_current_record_and_change_request(id: int):
+    pass
