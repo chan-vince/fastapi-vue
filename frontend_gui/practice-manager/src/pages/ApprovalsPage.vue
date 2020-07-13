6f2da7c5ba831a774fc2a5628701ef5298ccea99
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2020-07-13 15:21:34 +0100

    add new history approvals table view for non null approval rows

--- frontend_gui/practice-manager/src/pages/ApprovalsPage.vue
+++ frontend_gui/practice-manager/src/pages/ApprovalsPage.vue
@@ -1,71 +1,74 @@
 <template>
-  <div>
-    <NavBar ref="navBar" />
-    <div class="container" style="max-width: 1920px">
-      <div class="card" id="spacing-margins-card">
-        <div class="card-content">
-          <b-tabs id="spacing-margins-tabs">
-            <b-tab-item label="Pending">
-                <PendingApprovalsTable
-                  id="tables"
-                  ref="pendingEmployeeApprovals"
-                  v-bind:pendingOnly="true"
-                  @refresh="refreshTables"
-                  style="margin: 0px 20px 120px 20px"
-                />
-            </b-tab-item>
-            <b-tab-item label="History">
-                <!-- <PracticeApprovalsTable
-                  id="tables"
-                  ref="pendingPracticeApprovals"
-                  v-bind:pendingOnly="true"
-                  @refresh="refreshTables"
-                  style="margin: 0px 20px 120px 20px"
-                /> -->
-            </b-tab-item>
-          </b-tabs>
+    <div>
+        <NavBar ref="navBar"/>
+        <div class="container" style="max-width: 1920px">
+            <div class="card" id="spacing-margins-card">
+                <div class="card-content">
+                    <b-tabs id="spacing-margins-tabs">
+                        <b-tab-item label="Pending">
+                            <PendingApprovalsTable
+                                    id="tablesPending"
+                                    ref="pendingApprovalsTable"
+                                    v-bind:pendingOnly="true"
+                                    @refresh="refreshTables"
+                            />
+                        </b-tab-item>
+                        <b-tab-item label="History">
+                            <HistoryApprovalsTable
+                                    id="tablesHistory"
+                                    ref="historyApprovalsTable"
+                                    v-bind:pendingOnly="false"
+                                    @refresh="refreshTables"
+                            />
+                        </b-tab-item>
+                    </b-tabs>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-import NavBar from "../components/NavBar";
-import PendingApprovalsTable from "../components/PendingApprovalsTable";
+    import NavBar from "../components/NavBar";
+    import PendingApprovalsTable from "../components/PendingApprovalsTable";
+    import HistoryApprovalsTable from "../components/HistoryApprovalsTable";
 
-export default {
-  name: "ApprovalsPage",
-  components: {
-    NavBar,
-    PendingApprovalsTable
-  },
-  // data() {
-  // },
-  created() {
-    console.log("Approvals Page");
-  },
-  methods: {
-    refreshTables() {
-      console.log("Time to refresh!");
-      // this.$refs.pendingPracticeApprovals.getStagingPractices();
-      // this.$refs.historicPracticeApprovals.getStagingPractices();
-      // this.$refs.pendingEmployeeApprovals.getStagingEmployees();
-      // this.$refs.historicEmployeeApprovals.getStagingEmployees();
-      // this.$refs.navBar.getPendingApprovalsCount();
-    }
-  }
-};
+    export default {
+        name: "ApprovalsPage",
+        components: {
+            NavBar,
+            PendingApprovalsTable,
+            HistoryApprovalsTable
+        },
+        // data() {
+        // },
+        created() {
+            console.log("Approvals Page");
+        },
+        methods: {
+            refreshTables() {
+                console.log("Time to refresh!");
+                // this.$refs.pendingPracticeApprovals.getStagingPractices();
+                // this.$refs.historicPracticeApprovals.getStagingPractices();
+                // this.$refs.pendingApprovalsTable.getStagingEmployees();
+                // this.$refs.historicEmployeeApprovals.getStagingEmployees();
+                // this.$refs.navBar.getPendingApprovalsCount();
+            }
+        }
+    };
 </script>
 
 <style scoped>
-#spacing-margins-card {
-  margin: 10px 25px 25px 25px;
-}
-#spacing-margins-tabs {
-  margin: 0px 20px 40px 20px;
-}
-#tables{  
-  min-height: 600px;
-}
+    #spacing-margins-card {
+        margin: 10px 25px 25px 25px;
+    }
+
+    #spacing-margins-tabs {
+        margin: 0 20px 40px 20px;
+    }
+
+    #tablesPending, #tablesHistory {
+        min-height: 480px;
+        margin: 0 20px 120px 20px;
+    }
 </style>
